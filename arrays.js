7719398d14fc28a5448f6d2cfda3d34a5cc601e5
florenceNamukisa <florencenamukisa08@gmail.com>
2024-07-10 15:28:08 +0300

    added loops page

--- arrays.js
+++ arrays.js
@@ -1,16 +1,18 @@
 let shoppingList = ["bananas", "mangoes", "oranges"]
 console.log( "Here is shopping list:",shoppingList);
+let list=(shoppingList["avocados","apple"]);
+console.log("Here is   shopping list:", list);
 
-// const primeNumbers = [2,3,5,7,11]
-// console.log(primeNumbers);
+const primeNumbers = [2,3,5,7,11]
+console.log(primeNumbers);
 
-// let countries = ["Uganda", "Kenya", "Rwanda"]
-// console.log(countries)
+let countries = ["Uganda", "Kenya", "Rwanda"]
+console.log(countries)
 
-// let profile = ["Florence",
-//     25,
-//     true,
-//     {country: "uganda",city: "Kampala"},
-//     {Skills:["word",  "HTML", "CSS","JavaScript"]}
-//   ]
-//   console.log(profile);
+let profile = ["Florence",
+    25,
+    true,
+    {country: "uganda",city: "Kampala"},
+    {Skills:["word",  "HTML", "CSS","JavaScript"]}
+  ]
+  console.log(profile);
---
+++ loops.js
@@ -0,0 +1,29 @@
+// print 0-19 using console.log
+//let m =0;inirtialization
+for(let m = 0; m<=19; m++){
+    console.log(m)
+}
+for(let n = 19; n>=0;n--){
+    console.log(n)
+}
+
+
+let numbers = [1,2,3,4,5]
+for(let i = 0; i< numbers.length; i++){
+    let add = numbers[i]+20;
+    console.log(add)
+}
+//inirtialization, condition, increment/decrement
+let sum = 0
+for( i = 0; i<numbers.length; i++){
+sum = sum+numbers[i]
+}
+console.log(sum)
+// own trial
+for(let i=1; i<=5; i++){
+    console.log(`${i}.hello\n`)
+}
+ let names = ['florence','daina',' sharon']
+ for(let i = 0; i<names.length; i++){
+    console.log(names)
+ }
